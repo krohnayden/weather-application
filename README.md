8dce95989ede911f886831e9b1c1922c5b6ab89c
Ayden Krohn <krohnayden@gmail.com>
2023-10-30 18:29:27 -0500

    added weather info for forecast and added labels for values

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# weather-application
+# Weather-Application
+
--- script.js
+++ script.js
@@ -1,35 +1,19 @@
 var currentWeatherEl = document.querySelector("#currentWeather");
 var forecastEl = document.querySelector("#forecast");
-var cardEl = document.querySelector(".card");
-var cardImgEl = document.querySelector(".card-img-top");
 var cityInputEl = document.querySelector("#citySearch");
 var cityHistoryEl = document.querySelector("#cityHistory");
 var cityInput = document.getElementById('cityInput');
 var cityName = document.getElementById('cityName');
+var displayDate = document.getElementById('displayDate')
+var cardTitle = document.querySelectorAll('.card-title');
 var apiKey = '2dc6d899eaadebc826a5379a400f6d63';
+const currentDate = dayjs().format("MM/DD/YYYY");
+displayDate.innerHTML = `<h2 id="displayDate">${currentDate}</h2>
+`
 
-// Write function to display current weather based on the user input on city.
-
-// Write function to display 5-day forecast.
-
-// Write function to make 5-day forecast.
-// 1. 
-
-// Write function to make photos of cards change based on weather.
-// // 1. Make a series of if statements that change the img based on weather. 
-// // 2. Add img's on to cards(forecast). 
-
-// Write function to save history of searched cities.
-// // 1. Set user inputed city in local storage.
-// // 2. Get user inputed city to apply to list of recent searches.
-
-// Write function to find location in lat&long based off city name.
-// // 1. Run city name through API to get lat&long of location.
-// // 2. Use lat&long to get weather data from that location.
-// // 3. Use parameters in fetch request to narrow weather data collected.
-// // 4. Use data returned to apply to the current weather and 5-day forecast.
-
+// Function to apply weather information to HTML.
 function convertLocation(cityName) {
+  // Current weather API
   geolocationURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
   fetch(geolocationURL)
     .then(function (response) {
@@ -37,14 +21,16 @@ function convertLocation(cityName) {
     })
     .then(function (data) {
       console.log(data);
-      currentWeatherEl.innerHTML = `            <div id="currentWeather">
+      currentWeatherEl.innerHTML = `<div id="currentWeather">
       <h2>${data.name} (${dayjs.unix(data.dt).format("MM/DD/YYYY")})<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="#"></h2>
-      <p>Temp: ${data.main.temp}</p>
-      <p>Humidity: ${data.main.humidity}</p>
-      <p>Wind: ${data.wind.speed}</p>
+      <p>Temp: ${data.main.temp}&#176F</p>
+      <p>Humidity: ${data.main.humidity}%</p>
+      <p>Wind: ${data.wind.speed}(mph)</p>
     </div>
 `
+
     });
+    // Forecast API
   geolocationURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
   fetch(geolocationURL)
     .then(function (response) {
@@ -54,20 +40,20 @@ function convertLocation(cityName) {
       forecastEl.innerHTML = ""
       for (i = 2; i < data.list.length; i = i + 8) {
         console.log(data.list[i]);
-        forecastEl.innerHTML += `                <div class="card" id="card1" style="width: 18rem;">
-        <img class="card-img-top" src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="Card image cap">
+        forecastEl.innerHTML += `<div class="card" id="card1" style="width: 18rem;">
+        <img class="card-img-top" src="Images/thunderbolt-1905603_640.png" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">${data.list[i].dt_txt}</h5>
+          <p class="card-text">Temp: ${data.list[i].main.temp}&#176F</p>
+          <p class="card-text">Humidity: ${data.list[i].main.humidity}%</p>
+          <p class="card-text">Wind Speed: ${data.list[i].wind.speed}(mph)</p>
         </div>
-      </div> 
-`
+      </div>`
       }
     })
 }
 
-
+// Event listener to run function when city is entered.
 cityInput.addEventListener('submit', function (event) {
   event.preventDefault();
   convertLocation(cityName.value);
